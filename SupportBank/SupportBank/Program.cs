d7e43b01523ff229b9fc94703eeddabc0085da00
Joana Boiso-Ayan <J.Ayan@ucas.ac.uk>
2019-11-20 16:06:55 +0000

    SupportBank

--- SupportBank/SupportBank/Program.cs
+++ SupportBank/SupportBank/Program.cs
@@ -13,6 +13,7 @@ namespace SupportBank
         static void Main(string[] args)
         {
             // Get the file's text.
+           
             string [] Lines = File.ReadAllLines(@"../../../Transactions2014.csv");
 
             // Split into lines.
@@ -23,6 +24,16 @@ namespace SupportBank
             int num_rows = Lines.Length;
             int num_cols = Lines[0].Split(',').Length;
 
+            var transactionList = new Transaction[Lines.Length];
+
+            var transaction = new Transaction();
+            transaction.narrative = "hello";
+            transaction.amount = "700";
+            transaction.Date = "14/2/15";
+            transaction.From = "Jon";
+            transaction.To = "Amy";
+
+            transactionList[0] = transaction;
 
             // Allocate the data array.
             string[,] values = new string[num_rows, num_cols];
@@ -30,6 +41,7 @@ namespace SupportBank
             // Load the array.
             for (int r = 0; r < num_rows; r++)
             {
+
                 string[] line_r = Lines[r].Split(',');
                 for (int c = 0; c < num_cols; c++)
                 {
@@ -37,10 +49,36 @@ namespace SupportBank
                 }
             }
 
+
+
+            for (int i = 0; i < Lines.Length; i++)
+            {
+                if (transactionList[i] != null)
+                {
+                    Console.WriteLine(transactionList[i].Describe());
+                } 
+            }
+
             // Return the values.
-            Console.Write (values.ToString());
+            //Console.Write (values.ToString());
             Console.ReadLine();
         }
+
+        class Transaction
+        {
+            public string Date;
+            public string From;
+            public string To;
+            public string narrative;
+            public string amount;
+
+            public string Describe()
+            {
+                return $"A transaction from {From} to {To} for {amount}";
+            }
+        }
+
+       
 
 
     }
